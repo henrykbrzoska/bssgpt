c45e24737b30acd6a3a2ec0049f2bc0d1daade1b
henrykb <henryk.brzoska@gmail.com>
2024-02-12 10:42:03 +0100

    Update GptService.php
    cleanup

--- src/Service/GptService.php
+++ src/Service/GptService.php
@@ -7,10 +7,6 @@ class GptService
 {
     public function generateMetaData(Product $product)
     {
-        // Tutaj umieść logikę zapytania do API GPT
-        // Zwróć wygenerowane meta dane
-
-        // Przykład zwracanych danych
         return [
             'meta_title' => 'Wygenerowany Meta Title dla ' . $product->name,
             'meta_description' => 'Wygenerowany Meta Description dla ' . $product->name
